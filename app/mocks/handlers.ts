a49bd8ffbc03064461649985991030873541c48b
channy107 <hunchanlim@HUNCHANui-MacBookAir.local>
2023-12-14 13:08:25 +0900

    mocks 폴더 위치 변경

--- app/mocks/handlers.ts
+++
@@ -1,8 +0,0 @@
-import { HttpResponse, http } from 'msw';
-import { boardMenus } from './data';
-
-export const handlers = [
-  http.get('/api/boardMenus', () => {
-    return HttpResponse.json(boardMenus);
-  }),
-];
--- app/mocks/index.ts
+++
@@ -1,6 +0,0 @@
-export async function initMocks() {
-    if (typeof window !== 'undefined') {
-        const { worker } = await import('./browser');
-        worker.start();
-    } 
-}
